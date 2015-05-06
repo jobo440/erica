98933eed460370011dc500f2b0f05ae4d25818a3
jobo440 <gstajoe@gmail.com>
2015-05-06 12:47:31 -0700

    Add development, test, and  production groups in gemfile

--- app/controllers/erica_pages_controller.rb
+++ app/controllers/erica_pages_controller.rb
@@ -1,4 +1,26 @@
 class EricaPagesController < ApplicationController
+	def about
+	end
+
+	def blog
+	end
+
+	def contact
+	end
+
 	def home
 	end
+
+	def parent
+	end
+
+	def services
+	end
+
+	def teacher
+	end
+
+	def work
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
 root 'erica_pages#home'
+get 'blog' => 'erica_pages#blog'
+get 'teacher' => 'erica_pages#teacher'
+get 'parent' => 'erica_pages#parent'
+get 'work' => 'erica_pages#work'
+get 'services' => 'erica_pages#services'
+get 'contact' => 'erica_pages#contact'
+get 'about' => 'erica_pages#about'
 end
