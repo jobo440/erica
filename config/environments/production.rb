948244926d223f57bf65b14d4e78d17c3f4c269c
jobo440 <gstajoe@gmail.com>
2015-06-11 22:11:18 -0700

    add mailgun to actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Set up actionmailer with mailgun
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {domain: 'app123example.mailgun.org'}
 end
